5972fcef875cbd07a5a8b5c14c1c9741fcb17670
Luca Cavallin <lucavallin@github.com>
2022-07-09 22:02:56 +0200

    start work on auth subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(about = "Authenticate with a Philips Hue Bridge")]
+    Auth {
+        #[clap(value_parser)]
+        #[clap(help = "IP address of the Philips Hue Bridge to authenticate with")]
+        bridge_ip: Option<String>,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Auth { bridge_ip } => {
+            println!("Requested auth for Bridge IP: {:?}", bridge_ip);
+            let resp = reqwest::Client::new()
+                .post(format!("http://{}/api", bridge_ip.as_ref().unwrap()))
+                .send()
+                .await?
+                .json()
+                .await?;
+
+            println!("{:#?}", resp);
+            Ok(())
+        }
+    }
 }
